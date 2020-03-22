c2c18d8ae8642a01b4afa4d3bb156021cc7b1714
hesperion <hesperionxxi@hotmail.com>
2020-03-22 19:05:30 +0100

    Update editCours.js

--- commands/editCours.js
+++ commands/editCours.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const fs = require("fs");
 
 module.exports.run = async (bot, msg, args) =>{
-    if(!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send("pas la permission !");
+    if(!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send("Vous n'avez pas la permission d'effectuer cette action !");
 
     if(args[1] && args[2])
     {
@@ -13,7 +13,7 @@ module.exports.run = async (bot, msg, args) =>{
         {
             if(bot.cours[i].nom == args[1] && bot.cours[i].guild == msg.guild.id)
             {
-                bot.cours[i] = { //on ecrase les données
+                bot.cours[i] = { //on écrase les données
                     guild: msg.guild.id,
                     contenu: content,
                     nom: args[1],
@@ -23,23 +23,23 @@ module.exports.run = async (bot, msg, args) =>{
                 await fs.writeFile("./cours/last.json", JSON.stringify(bot.cours, null, 4), err =>{ //on les sauvegarde
                     if(err) throw err;
                 
-                    msg.channel.send(`c'est bon ! nouveau contenu => \`${content.join().replace(/,/g, " ")}\` , \`!math dumpCours\` pour afficher les cours`);
+                    msg.channel.send(`Nouveau contenu du cours du **${bot.cours[i].nom}** : \`${content.join().replace(/,/g, " ")}\``);
                 });
                 k+=1; //si ca trouve un cours avec le bon nom
             }
         } // on retouve le cours en question
 
-        if(k === 0) return msg.channel.send(`pas de modifs faites car pas de cours nommé ${args[1]}`); // message d'erreur si k = 0 car ca veut dire que acun msg n'a été trouvé !
+        if(k === 0) return msg.channel.send(`Pas de modificaton car pas de cours nommé ${args[1]}`); // message d'erreur si k = 0 car ca veut dire que acun msg n'a été trouvé !
 
         return;
     }
     else
     {
-        msg.channel.send("veuillez bien tout preciser (!SI help pour + d'infos");
+        msg.channel.send("Veuillez bien tout préciser (\`!math help\` pour plus d'infos) !");
     }
 }
 
 module.exports.help = {
     name: "editCours",
-    desc: "`pour modifier des anciens cours\n!math modifCours <nom> <contenu>`"
+    desc: "`Pour modifier des anciens cours\n!math modifCours <nom> <contenu du cours>`"
 }
