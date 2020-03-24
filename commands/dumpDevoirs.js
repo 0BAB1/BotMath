ad84e4e9cfb0dd1dea42454ac7a36dd6fa08e5ac
0BAB1 <hb.hugobabin@gmail.com>
2020-03-24 13:43:44 +0100

    test2

--- commands/dumpDevoirs.js
+++ commands/dumpDevoirs.js
@@ -4,60 +4,69 @@ const fs = require("fs");
 module.exports.run = async (bot, msg, args) =>{
     let guildId = msg.guild.id;
     let devoirsArray = new Array;
-    let stringDevoir = new String;
+    let stringDevoirs = new String;
+    let k = new String;
+    let allDevoirs = new String;
 
     //===================================//
     //=====afficher le dernier cours=====//
     //===================================//
-    
     if(args[1] === "last")
     {
         for(let i in bot.devoirs){
-            stringDevoir = bot.devoirs[i].contenu.join(" "); // traitement pour avoir une belle string a afficher
-            if(bot.devoirs[i].guild == guildId && bot.devoirs[i].channel == msg.channel.id) devoirsArray.push(stringDevoir);var nom = bot.devoirs[i].nom; //on fait un tableau dans lequel se trouvent les cours du salon dans lequel a été envoyé la commande.
-        }//le petit var nom prend la valeur du nom du dernier devoir pour l'affcher ensuite, pas tres opti comme partie, a refaire eventuellement
-
-        if(devoirsArray.length > 0) msg.channel.send(`devoirs pour le : **${nom}** ===> *${devoirsArray[devoirsArray.length - 1]}*`);
-        return;
-    }
-    
-    else if(!args[1])
-    {
-        for(let i in bot.devoirs){
-            if(bot.devoirs[i].guild == guildId && bot.devoirs[i].channel == msg.channel.id) 
-            {
-                stringDevoir = bot.devoirs[i].contenu.join(" "); // traitement pour avoir une belle string a afficher
-                msg.channel.send(`Devoirs pour le **${bot.devoirs[i].nom}** : *${stringDevoir}*`)
+            if(bot.devoirs[i].guild == guildId && bot.devoirss[i].channel == msg.channel.id) {
+                stringDevoirs = bot.devoirs[i].contenu.join(" "); // traitement pour avoir une belle string a afficher
+                k = i; //on sauvegarde l'id du dernier cours trouvé
             }
         }
-    }
 
+        if(stringCours.length > 0) {
+            msg.channel.send(`Devoirs du **${bot.devoirs[k].nom}** : *${stringDevoirs}*`);
+        } else {
+            msg.channel.send(`Aucune entrée n'a encore été saisie !`); //pour insulter l'utilisateur
+        }
+        return;
+    }
     //===================================//
     //===========tout afficher===========//
     //===================================//
-
+    else if(!args[1])
+    {
+        for(let i in bot.devoirs){
+            if(bot.devoirs[i].guild == guildId && bot.devoirs[i].channel == msg.channel.id && bot.devoirs[i].guild == msg.guild.id) 
+            {
+                stringDevoirs = bot.devoirs[i].contenu.join(" "); // traitement pour avoir une belle string a afficher
+                allDevoirs += `Devoirs du **${bot.devoirs[i].nom}** : *${stringDevoirs}*` + "\n"; //on concatène tous les cours
+            }
+        }
+        msg.channel.send(allDevoirs); //on affiche en une seule fois pour éviter de multiples alertes
+    }
+    //===================================//
+    //======afficher le cours voulu======//
+    //===================================//
     else
     {
         let nomAffiche = args[1];
-        let devoirsArray = new Array;
-        let stringDevoir = new String;
         
         for(let i in bot.devoirs){
-            stringDevoir = bot.devoirs[i].contenu.join(" "); // traitement pour avoir une belle string a afficher
-            if(bot.devoirs[i].guild == guildId && bot.devoirs[i].channel == msg.channel.id && bot.devoirs[i].nom == nomAffiche) devoirsArray.push(stringDevoir); //on fait un tableau dans lequel se trouvent les cours du salon dans lequel a été envoyé la commande.
+            if(bot.devoirs[i].guild == guildId && bot.devoirs[i].channel == msg.channel.id && bot.devoirs[i].nom == nomAffiche) {
+                stringCDevoirs = bot.devoirs[i].contenu.join(" "); // traitement pour avoir une belle string a afficher
+                k = i; //on sauvegarde l'id du cours trouvé
+                break; //pas la peine de continuer à chercher une entrée, on l'a déjà trouvée
+            }
         }
 
-        if(devoirsArray.length > 0) {
-            msg.channel.send(devoirsArray);
+        if(stringDevoirs.length > 0) {
+            msg.channel.send(`Cours du **${bot.devoirs[k].nom}** : *${stringDevoirs}*`);
         }
         else{
-            msg.channel.send(`Aucun cours nommé ${nomAffiche}`);
+            msg.channel.send(`Aucun cours nommé **${nomAffiche}**`);
         }
         return;
     }
 }
 
 module.exports.help = {
-    name: "dumpCours",
-    desc: "`!math dumpCours last : affiche le dernier cours\n!math dumpCours <nom> : affiche le cours nommé\n!math dumpCours : affiche tous les cours`"
+    name: "dumpDevoirs",
+    desc: "`Pour afficher un ou des des devoirs.\n!math dumpDevoirs last : affiche le dernier devoir a faire.\n!math dumpDevoirs <nom> : affiche le devoir nommé.\n!math dumpDevoirs : affiche tous les devoirs.`"
 }
