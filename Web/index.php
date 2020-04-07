67080884faa5e9d902224f9bf40dc2a94bdaf0d6
0BAB1 <hb.hugobabin@gmail.com>
2020-04-07 20:53:42 +0200

    ajout d'élément web

---
+++ Web/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Bot Manager</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="subBody">
+        <div id="header">
+            <img src="images/header.png" width=85 height=85>
+            <h1>Bot Math Manager by M. and initial</h1>
+        </div>
+        <div id="main">
+            <div id="menu">
+                <ul>
+                    <li><a href="#">accueil</a></li>
+                    <li><a href="#">cours</a></li>
+                    <li><a href="#">devoirs</a></li>
+                    <li><a href="#">fichiers</a></li>
+                    <li><a href="#">compte</a></li>
+                </ul>
+            </div>
+            <div id="content">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ start.sh
@@ -0,0 +1,4 @@
+sudo rsync -r Web/ /var/www/html/
+echo Site Web actualisé !
+echo
+node index.js
